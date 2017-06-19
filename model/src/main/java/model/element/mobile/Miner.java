a593d313b5d021260a1744a64973b3c61a173c24
lauraasld <laura.schlauder@viacesi.fr>
2017-06-19 10:31:16 +0200

    stub code

---
+++ model/src/main/java/model/element/mobile/Miner.java
@@ -0,0 +1,45 @@
+package model.element.mobile;
+
+import model.IMap;
+import model.element.Sprite;
+
+public class Miner {
+	private Sprite	sprite1;
+	private Sprite	sprite2;
+	private Sprite	spriteRight1;
+	private Sprite	spriteRight2;
+	private Sprite	spriteDown1;
+	private Sprite	spriteDown2;
+	private Sprite	spriteUp1;
+	private Sprite	spriteUp2;
+	private Sprite	spriteLeft1;
+	private Sprite	spriteLeft2;
+
+	public Miner(int x, int y, IMap map) {
+
+	}
+
+	public void moveUp() {
+
+	}
+
+	public void moveDown() {
+
+	}
+
+	public void moveRight() {
+
+	}
+
+	public void moveLeft() {
+
+	}
+
+	protected void die() {
+
+	}
+
+	public void doNothing() {
+
+	}
+}
