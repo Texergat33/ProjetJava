8cb4a1e71c03b4c9cd46967e115f8f0e83624ff1
lauraasld <laura.schlauder@viacesi.fr>
2017-06-19 10:20:59 +0200

    stub code

---
+++ model/src/main/java/model/element/Element.java
@@ -0,0 +1,37 @@
+package model.element;
+
+import java.awt.Image;
+
+public abstract class Element {
+	private Permeability	permeability;
+	// private Sprite sprite;
+
+	private Sprite			sprite	= new Sprite(null);
+
+	public Element(Sprite sprite, Permeability permeability) {
+
+	}
+
+	public Sprite getSprite() {
+		return this.sprite;
+	}
+
+	public Permeability getPermeability() {
+		return this.permeability;
+
+	}
+
+	public Image getImage() {
+		return null;
+
+	}
+
+	protected void setSprite(Sprite sprite) {
+		this.sprite = sprite;
+	}
+
+	private void setPermeability(Permeability permeability) {
+		this.permeability = permeability;
+	}
+
+}
---
+++ model/src/main/java/model/element/mobile/Mobile.java
@@ -0,0 +1,72 @@
+package model.element.mobile;
+
+import java.awt.Point;
+
+import model.element.Permeability;
+import model.element.Sprite;
+
+public abstract class Mobile implements IMobile {
+	private Point	position;
+	private boolean	alive	= true;
+	private IBoard	board;
+
+	public Mobile(Point position, Sprite sprite, IMap map, Permeability permeability) {
+
+	}
+
+	private void setMobileHasChanged() {
+
+	}
+
+	@Override
+	public void moveUp() {
+
+	}
+
+	@Override
+	public void moveDown() {
+
+	}
+
+	@Override
+	public void moveRight() {
+
+	}
+
+	@Override
+	public void moveLeft() {
+
+	}
+
+	@Override
+	public void doNothing() {
+
+	}
+
+	@Override
+	public boolean isALive() {
+
+	}
+
+	@Override
+	public boolean isKilled() {
+
+	}
+
+	@Override
+	public Point getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(Point position) {
+		this.position = position;
+	}
+
+	protected IBoard getBoard() {
+
+	}
+
+	protected void die() {
+
+	}
+}
