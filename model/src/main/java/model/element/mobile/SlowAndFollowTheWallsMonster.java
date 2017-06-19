2c8d402d68cf95daae87a50e50df82d4eef32389
Agathe <Agathe@10.133.129.7>
2017-06-19 14:42:13 +0200

    Stub code - Addition of the DP Strategy

---
+++ model/src/main/java/model/element/mobile/SlowAndFollowTheWallsMonster.java
@@ -0,0 +1,13 @@
+package model.element.mobile;
+
+import model.element.Permeability;
+import model.element.Sprite;
+
+public class SlowAndFollowTheWallsMonster extends AbstractMonster {
+
+	public SlowAndFollowTheWallsMonster(Sprite sprite, Permeability permeability) {
+		super(sprite, permeability);
+		// TODO Auto-generated constructor stub
+	}
+
+}
