180fce8ad269547e3fb5375b1b468d10b7890a20
laure_anne <laureanne.marechal@viacesi.fr>
2017-06-20 10:23:00 +0200

    Add methods which call the procedure

--- model/src/main/java/model/BoulderDashModel.java
+++ model/src/main/java/model/BoulderDashModel.java
@@ -1,31 +1,48 @@
 package model;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import model.dao.DAO;
 import model.element.mobile.IMobile;
 
-public class BoulderDashModel implements IBoulderDashModel{
-	private IMobile miner;
-	private IMap map;
-	public BoulderDashModel(int level, int minerStartX, int minerStartY){
+public class BoulderDashModel implements IBoulderDashModel, IModel {
+    private IMobile miner;
+    private IMap    map;
 
-	}
+    public BoulderDashModel(final int level, final int minerStartX, final int minerStartY) {
 
-	@Override
-	public void getMap() {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void getMap() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public IMobile getMiner() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	private void setMap(IMap map){
+    @Override
+    public IMobile getMiner() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    private void setMap(final IMap map) {
 
-	private void setMiner(IMobile miner){
+    }
 
-	}
+    private void setMiner(final IMobile miner) {
+
+    }
+
+    @Override
+    public MapDimensions getMapSize(final int id) throws SQLException {
+        return DAO.getMapSize(id);
+
+    }
+
+    @Override
+    public List<FillingMap> getMapFilled(final int id) throws SQLException {
+        return DAO.getMapFilled(id);
+
+    }
 }
