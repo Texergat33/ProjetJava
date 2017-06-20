045dfb6fb3b7e6ba0bb53e72f8c59abcf1f39cfa
Agathe <Agathe@10.133.129.7>
2017-06-20 16:52:18 +0200

    Sprite factory - Filling of methods

--- model/src/main/java/model/element/SpriteFactory.java
+++ model/src/main/java/model/element/SpriteFactory.java
@@ -1,7 +1,5 @@
 package model.element;
 
-import model.element.mobile.Monster;
-
 public class SpriteFactory {
 	private static Sprite monsterFAFTW = new Sprite();
 	private static Sprite monsterSAFTW = new Sprite();
@@ -34,18 +32,81 @@ public class SpriteFactory {
 	};
 
 	public Sprite createBoulder(){
-
+		return boulder;
 	}
 
-	public Sprite createDiamond(){
-
+	public Sprite createDiamond(int front){
+		switch(front){
+		case 1:
+			return diamondFront1;
+			break;
+		case 2:
+			return diamondFront2;
+			break;
+		default :
+			return diamondFront1;
+		}
 	}
 
-	public Sprite createMiner(){
-
+	public Sprite createMiner(String minerPosition){
+		switch(minerPosition)
+	case "defaultMiner1":
+		return defaultMiner1;
+		break;
+	case "defaultMiner2":
+		return defaultMiner2;
+		break;
+	case "minerUp1":
+		return minerUp1;
+		break;
+	case "minerUp2":
+		return minerUp2;
+		break;
+	case "minerDown1":
+		return minerDown1;
+		break;
+	case "minerDown2":
+		return minerDown2;
+		break;
+	case "minerLeft1":
+		return minerLeft1;
+		break;
+	case "minerLeft2":
+		return minerLeft2;
+		break;
+	case "minerRight1":
+		return minerRight1;
+		break;
+	case "minerRight2":
+		return minerRight2;
+		break;
+	case "deadMiner1":
+		return deadMiner1;
+		break;
+	case "deadMiner2":
+		return deadMiner2;
+		break;
+	default:
+		return defaultMiner1;
 	}
 
-	public Sprite createMonster(){
-		switch()
+	public Sprite createMonster(String monsterName){
+		switch(monsterName){
+		case "monsterFAFTW":
+			return monsterFAFTW;
+			break;
+		case "monsterSAFTW":
+			return monsterSAFTW;
+			break;
+		case "monsterFAR":
+			return monsterFAR;
+			break;
+		case "monsterSAR":
+			return monsterSAR;
+			break;
+		default:
+			return monsterSAR;
+			break;
+		}
 	}
 }
