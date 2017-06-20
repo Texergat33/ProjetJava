162be10b41e5215f7daf2f1565e347342d64cc1e
Agathe <Agathe@10.133.129.7>
2017-06-20 11:54:07 +0200

    Stub code - Sprite Factory (need modifications)

--- model/src/main/java/model/element/SpriteFactory.java
+++ model/src/main/java/model/element/SpriteFactory.java
@@ -1,5 +1,19 @@
 package model.element;
 
-public class SpriteFactory {
+import model.element.mobile.Boulder;
+import model.element.mobile.Diamond;
+import model.element.mobile.Miner;
 
+public class SpriteFactory {
+	private static Monster monster = new Monster();
+	private static Boulder boulder = new Boulder(null, null, null, null);
+	private static Miner miner = new Miner(null, null, null, null);
+	private static Diamond diamond = new Diamond(null, null, null, null);
+
+	private static Sprite[] sprites = {
+			monster,
+			boulder,
+			miner,
+			diamond
+	};
 }
