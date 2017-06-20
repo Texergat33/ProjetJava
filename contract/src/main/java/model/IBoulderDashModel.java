4ab002e59f38d6ad1ce3ddb3c6519ad57f5d545a
Agathe <Agathe@10.133.129.7>
2017-06-20 14:05:36 +0200

    Filling BoulderDashModel

--- contract/src/main/java/model/IBoulderDashModel.java
+++ contract/src/main/java/model/IBoulderDashModel.java
@@ -1,7 +1,5 @@
 package model;
 
-import model.element.mobile.IMobile;
-
 public interface IBoulderDashModel {
 	public IMap getMap();
 	public IMobile getMiner();
--- model/src/main/java/model/BoulderDashModel.java
+++ model/src/main/java/model/BoulderDashModel.java
@@ -1,14 +1,15 @@
 package model;
 
 import model.element.mobile.IMobile;
+import model.element.mobile.Miner;
 
 public class BoulderDashModel implements IBoulderDashModel{
 	private IMobile miner;
 	private IMap map;
 	public BoulderDashModel(int level, int minerStartX, int minerStartY){
-
+		this.setMap(new Map(level));
 		//met en place la route en créant la Map avec en paramètre le numéro de Map
-
+		this.setMiner(new Miner(minerStartY, minerStartY, this.getMap()));
 		//met en place le mineur en le créant avec sa position X et Y de départ, et récupère la Map
 	}
 
