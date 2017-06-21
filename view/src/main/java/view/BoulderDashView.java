7c68f8551f6d1d80e828701ecc8c950654555719
laure_anne <laureanne.marechal@viacesi.fr>
2017-06-21 13:48:34 +0200

    Code View 4

--- view/src/main/java/view/BoulderDashView.java
+++ view/src/main/java/view/BoulderDashView.java
@@ -20,6 +20,7 @@ import model.element.IElement;
 import model.element.mobile.IMobile;
 import model.element.mobile.Miner;
 
+// TODO: Auto-generated Javadoc
 /**
  * <h1>The Class ViewFacade provides a facade of the View component.</h1>
  *
@@ -27,24 +28,49 @@ import model.element.mobile.Miner;
  * @version 1.0
  */
 public class BoulderDashView implements IBoulderDashView, Runnable, KeyListener, Observer {
-    private static final int    squareSize  = 10;
-    private static int          boulderView = 10;
-    public int                  view;
-    private IOrderPerformer     orderPerformer;
-    private Miner               miner;
-    private IMobile             mobile;
-    private IMap                map;
-    private Rectangle           closeView;
+
+    /** The Constant squareSize. */
+    private static final int squareSize = 10;
+
+    /** The boulder view. */
+    private static int boulderView = 10;
+
+    /** The view. */
+    public int view;
+
+    /** The order performer. */
+    private IOrderPerformer orderPerformer;
+
+    /** The miner. */
+    private Miner miner;
+
+    /** The mobile. */
+    private IMobile mobile;
+
+    /** The map. */
+    private IMap map;
+
+    /** The close view. */
+    private Rectangle closeView;
+
+    /** The objects. */
     private ArrayList<IElement> objects;
 
     /**
-     * Instantiates a new view facade.
+     * Instantiates a new boulder dash view.
+     *
+     * @param map
+     *            the map
+     * @param miner
+     *            the miner
+     * @param objects
+     *            the objects
      */
     public BoulderDashView(final IMap map, final Miner miner, final ArrayList<IElement> objects) {
         final ArrayList<IElement> mobiles = new ArrayList<IElement>();
         this.setMiner(miner);
         this.setMap(map);
-        this.getMiner().getSprite().loadImage();
+        this.getMiner().getPosition();
         this.setCloseView(new Rectangle(this.getMiner().getX(), this.getMiner().getY(), BoulderDashView.boulderView,
                 BoulderDashView.boulderView));
         SwingUtilities.invokeLater(this);
@@ -56,13 +82,20 @@ public class BoulderDashView implements IBoulderDashView, Runnable, KeyListener,
      *
      * @see view.IView#displayMessage(java.lang.String)
      */
+    /* this method will be used for display the "game over" message */
     @Override
     public final void displayMessage(final String message) {
         JOptionPane.showMessageDialog(null, message);
     }
 
     /*
+     * (non-Javadoc)
+     *
+     * @see java.lang.Runnable#run()
+     */
+    /*
      * the method will display on the screen the different element who constite
+     * the game
      */
     @Override
     public void run() {
@@ -93,6 +126,13 @@ public class BoulderDashView implements IBoulderDashView, Runnable, KeyListener,
      * }
      */
 
+    /**
+     * Key code to user order.
+     *
+     * @param keyCode
+     *            the key code
+     * @return the user order
+     */
     private static UserOrder keyCodeToUserOrder(final int keyCode) {
         UserOrder userOrder;
         switch (keyCode) {
@@ -109,39 +149,90 @@ public class BoulderDashView implements IBoulderDashView, Runnable, KeyListener,
         return userOrder;
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see view.IBoulderDashView#followMiner()
+     */
+
+    /* this method will make the view follow the player. */
     @Override
     public void followMiner() {
         this.closeView = new Rectangle(this.getMiner().getX(), this.getMiner().getY());
     }
 
+    /**
+     * Gets the view.
+     *
+     * @return the view
+     */
     public int getView() {
         return this.view;
     }
 
+    /**
+     * Sets the view.
+     *
+     * @param view
+     *            the new view
+     */
     public void setView(final int view) {
         this.view = view;
     }
 
+    /**
+     * Gets the order performer.
+     *
+     * @return the order performer
+     */
     public IOrderPerformer getOrderPerformer() {
         return this.orderPerformer;
     }
 
+    /**
+     * Sets the order performer.
+     *
+     * @param orderPerformer
+     *            the new order performer
+     */
     public void setOrderPerformer(final IOrderPerformer orderPerformer) {
         this.orderPerformer = orderPerformer;
     }
 
+    /**
+     * Gets the miner.
+     *
+     * @return the miner
+     */
     public IMobile getMiner() {
         return this.miner;
     }
 
+    /**
+     * Sets the miner.
+     *
+     * @param miner
+     *            the new miner
+     */
     public void setMiner(final Miner miner) {
         this.miner = miner;
     }
 
+    /**
+     * Gets the map.
+     *
+     * @return the map
+     */
     public IMap getMap() {
         return this.map;
     }
 
+    /**
+     * Sets the map.
+     *
+     * @param map
+     *            the new map
+     */
     public void setMap(final IMap map) {
         this.map = map;
         for (int x = 0; x < this.getMap().getWidth(); x++) {
@@ -151,42 +242,84 @@ public class BoulderDashView implements IBoulderDashView, Runnable, KeyListener,
         }
     }
 
+    /**
+     * Gets the close view.
+     *
+     * @return the close view
+     */
     public Rectangle getCloseView() {
         return this.closeView;
     }
 
+    /**
+     * Sets the close view.
+     *
+     * @param closeView
+     *            the new close view
+     */
     public void setCloseView(final Rectangle closeView) {
         this.closeView = closeView;
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
+     */
     @Override
     public void keyPressed(final KeyEvent e) {
         this.getOrderPerformer().orderPerform(BoulderDashView.keyCodeToUserOrder(e.getKeyCode()));
 
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+     */
     @Override
     public void update(final Observable o, final Object arg) {
         // TODO Auto-generated method stub
 
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.awt.event.KeyListener#keyTyped(java.awt.event.KeyEvent)
+     */
     @Override
     public void keyTyped(final KeyEvent e) {
         // TODO Auto-generated method stub
 
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.awt.event.KeyListener#keyReleased(java.awt.event.KeyEvent)
+     */
     @Override
     public void keyReleased(final KeyEvent e) {
         // TODO Auto-generated method stub
 
     }
 
+    /**
+     * Gets the mobile.
+     *
+     * @return the mobile
+     */
     public IMobile getMobile() {
         return this.mobile;
     }
 
+    /**
+     * Sets the mobile.
+     *
+     * @param mobile
+     *            the new mobile
+     */
     public void setMobile(final IMobile mobile) {
         this.mobile = mobile;
         for (final IElement elements : this.objects) {
