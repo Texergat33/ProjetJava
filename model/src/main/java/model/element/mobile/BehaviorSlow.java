78a4bdc4bf3941edaa90dfcfed147d7cc2f10747
lauraasld <laura.schlauder@viacesi.fr>
2017-06-21 21:29:43 +0200

    update BehaviorSlow

--- model/src/main/java/model/element/mobile/BehaviorSlow.java
+++ model/src/main/java/model/element/mobile/BehaviorSlow.java
@@ -1,48 +1,38 @@
 package model.element.mobile;
 
+import model.element.Permeability;
+
 public class BehaviorSlow extends BehaviorDoSomething {
 	int position;
 
 	@Override
-	/*-public void other() {
+	public void doSomething() {
 		int x = this.mobile.getX();
 		int y = this.mobile.getY();
-	
-		if (this.mobile.getMap().getOnTheMapXY(x, y + 1).getPermeability() == Permeability.PENETRABLEFORMONSTER) {
-			this.direction();
-			this.mobile.moveDown();
-		} else if (this.mobile.getMap().getOnTheMapXY(x, y - 1)
-				.getPermeability() == Permeability.PENETRABLEFORMONSTER) {
-			this.direction();
-			this.mobile.moveUp();
-		} else if (this.mobile.getMap().getOnTheMapXY(x + 1, y)
-				.getPermeability() == Permeability.PENETRABLEFORMONSTER) {
-			this.direction();
-			this.mobile.moveRight();
-		} else if (this.mobile.getMap().getOnTheMapXY(x - 1, y)
-				.getPermeability() == Permeability.PENETRABLEFORMONSTER) {
-			this.direction();
-			this.mobile.moveLeft();
-		} else {
-			this.mobile.doNothing();
-		}
-	
-	}*/
 
-	public void doSomething() {
 		switch (this.random(1, 4)) {
 		case 1:
-			this.position = this.mobile.getX() - 1;
+			if (this.mobile.getMap().getOnTheMapXY(x, y + 1).getPermeability() == Permeability.PENETRABLEFORMONSTER) {
+				this.mobile.moveDown();
+			}
 			break;
 		case 2:
-			this.position = this.mobile.getX() + 1;
+			if (this.mobile.getMap().getOnTheMapXY(x, y - 1).getPermeability() == Permeability.PENETRABLEFORMONSTER) {
+				this.mobile.moveUp();
+			}
 			break;
 		case 3:
-			this.position = this.mobile.getY() + 1;
+			if (this.mobile.getMap().getOnTheMapXY(x + 1, y).getPermeability() == Permeability.PENETRABLEFORMONSTER) {
+				this.mobile.moveRight();
+			}
 			break;
 		case 4:
-			this.position = this.mobile.getY() - 1;
+			if (this.mobile.getMap().getOnTheMapXY(x - 1, y).getPermeability() == Permeability.PENETRABLEFORMONSTER) {
+				this.mobile.moveLeft();
+			}
 			break;
+		default:
+			this.mobile.doNothing();
 		}
 	}
 
