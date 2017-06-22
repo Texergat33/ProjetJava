bcff2d3fdba98c8e9276db84b5977dae0fe069b8
AgatheLagache <agathe.lagache@viacesi.fr>
2017-06-22 13:20:52 +0200

    Resolve errors

---
+++ model/src/main/java/model/IMap.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.Observable;
+
+import model.element.IElement;
+
+public interface IMap {
+    public int getHeight();
+
+    public Observable getObservable();
+
+    public IElement getOnTheMapXY(int x, int y);
+
+    public int getWidth();
+
+    public void setMobileHasChanged();
+}
