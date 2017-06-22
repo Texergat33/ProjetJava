786b3a2fe3adaead4e53f22ddf1581d5384e69b5
AgatheLagache <agathe.lagache@viacesi.fr>
2017-06-22 13:13:23 +0200

    Resolve errors

--- contract/src/main/java/controller/IBoulderDashController.java
+++ contract/src/main/java/controller/IBoulderDashController.java
@@ -1,7 +1,7 @@
 package controller;
 
 public interface IBoulderDashController {
-    public void play();
+    public void play() throws InterruptedException;
 
     public IOrderPerformer getOrderPerformer();
 
