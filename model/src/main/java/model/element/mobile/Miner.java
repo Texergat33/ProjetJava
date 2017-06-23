2525a433bac0e89ac720e7c877b7d47d4d4b0371
Texergat <gatien.texereau@viacesi.fr>
2017-06-23 09:27:48 +0200

    Miner's moves

--- model/src/main/java/model/element/mobile/Miner.java
+++ model/src/main/java/model/element/mobile/Miner.java
@@ -47,49 +47,62 @@ public class Miner extends Mobile {
 			this.setSprite(SpriteFactory.createMiner("defaultMiner1"));
 		}
 	}
-/*- Pour le déplacement, dans chaque méthode, il faut tester la permeabilité de l'objet dans la direction dans laquelle on veut 
- * aller. Ca va beaucoup ressembler aux déplacement des monstres. 
- * Pour pousser un rocher, il faut faire plusieurs tests (CHEZ LE MINEUR, PAS LE ROCHER, ou alors il faut appeller une fonction du rocher depuis le 
- * mineur, c'est chiant). Le premier test, c'est celle de la perméabilité de l'objet en face. Encore. Si c'est celle d'un rocher, faut tester celle 
- * de la case juste après. Si le tout est faisable, tu bouges le rocher d'une case dans la direction voulue, puis le mineur, puis tu lances
- * les méthodes fall et glide du rocher (qui ne font rien si ce n'est pas possible). 
- * VOILA VOILA. Encore désolé pour mes conneries les filles ^^'
- * (non-Javadoc)
- * @see model.element.mobile.Mobile#moveDown()
- */
+
 	@Override
 	public void moveDown() {
 			this.setSprite(SpriteFactory.createMiner("minerDown1"));
-		if (this.getMap().getOnTheMapXY(this.getY(), this.getY() + 1).getPermeability() == Permeability.PENETRABLEFORMINER) {
+		if (this.getMap().getOnTheMapXY(this.getX(), this.getY() + 1).getPermeability() == Permeability.PENETRABLEFORMINER) {
 			 this.setY(this.getY() + 1);
-		        this.setHasChanged();
 		}
+		else{
+			this.doNothing();
+		}
+		this.setHasChanged();
 	}
 
 	@Override
 	public void moveLeft() {
-		super.moveLeft();
-		while (true) {
-			this.setSprite(SpriteFactory.createMiner("minerLeft1"));
-
+		this.setSprite(SpriteFactory.createMiner("minerLeft1"));
+		if (this.getMap().getOnTheMapXY(this.getX() -1, this.getY()).getPermeability() == Permeability.PENETRABLEFORMINER) {
+			 this.setX(this.getX() - 1);		        
 		}
+		else if (this.getMap().getOnTheMapXY(this.getX() -1, this.getY()).getPermeability() == Permeability.PUSHABLE && 
+				this.getMap().getOnTheMapXY(this.getX() -2, this.getY()).getPermeability() == Permeability.PENETRABLEFOROTHERS) {
+			 this.getMap().getMobileXY(this.getX() - 1, this.getY()).moveLeft();
+			 this.setX(this.getX() - 1);
+		}
+		else{
+			this.doNothing();
+		}
+		this.setHasChanged();
 	}
-
 	@Override
 	public void moveRight() {
-		super.moveRight();
-		while (true) {
-			this.setSprite(SpriteFactory.createMiner("minerRight1"));
-
+		this.setSprite(SpriteFactory.createMiner("minerRight1"));
+		if (this.getMap().getOnTheMapXY(this.getX() + 1, this.getY()).getPermeability() == Permeability.PENETRABLEFORMINER) {
+			 this.setX(this.getX() + 1);		        
 		}
+		else if (this.getMap().getOnTheMapXY(this.getX() +1, this.getY()).getPermeability() == Permeability.PUSHABLE && 
+				this.getMap().getOnTheMapXY(this.getX() +2, this.getY()).getPermeability() == Permeability.PENETRABLEFOROTHERS) {
+			 this.getMap().getMobileXY(this.getX() + 1, this.getY()).moveRight();
+			 this.setX(this.getX() + 1);
+		}
+		else{
+			this.doNothing();
+		}
+		this.setHasChanged();
 	}
 
 	@Override
 	public void moveUp() {
-		super.moveUp();
-		while (true) {
-			this.setSprite(SpriteFactory.createMiner("minerUp1"));
+		this.setSprite(SpriteFactory.createMiner("minerUp1"));
+		if (this.getMap().getOnTheMapXY(this.getX(), this.getY() -1).getPermeability() == Permeability.PENETRABLEFORMINER) {
+			 this.setY(this.getY() - 1);		        
 		}
+		else{
+			this.doNothing();
+		}
+		this.setHasChanged();
 	}
 
 }
