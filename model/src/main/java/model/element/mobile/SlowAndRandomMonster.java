4c252d6029988454fc8e41aaada8cfbe5ef4c704
lauraasld <laura.schlauder@viacesi.fr>
2017-06-23 09:12:28 +0200

    update behavior monster

--- model/src/main/java/model/element/mobile/SlowAndRandomMonster.java
+++ model/src/main/java/model/element/mobile/SlowAndRandomMonster.java
@@ -1,12 +1,49 @@
 package model.element.mobile;
 
+import java.awt.Point;
+
+import model.IMap;
 import model.element.Permeability;
 import model.element.Sprite;
+import model.element.motionless.MotionlessElement;
 
 public class SlowAndRandomMonster extends Monster {
+
 	private static final Sprite SPRITE = new Sprite(7, "monster4");
 
-	public SlowAndRandomMonster(Sprite sprite, Permeability permeability) {
-		super(this.position, SPRITE, this.map, permeability);
+	public SlowAndRandomMonster(Point position, Sprite sprite, IMap map, Permeability permeability) {
+		super(position, SPRITE, map, permeability);
+		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public void collect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Point getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IMobile position(int i, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void shift(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public MotionlessElement createBackground() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
