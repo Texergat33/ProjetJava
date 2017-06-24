ea27ece6ca0d28283f078f181e5866077b751b9a
laure_anne <laureanne.marechal@viacesi.fr>
2017-06-24 19:00:20 +0200

    add Javadoc

--- model/src/main/java/model/element/Element.java
+++ model/src/main/java/model/element/Element.java
@@ -2,37 +2,79 @@ package model.element;
 
 import java.awt.Image;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Element.
+ */
 public abstract class Element implements IElement {
 
-	private Permeability	permeability;
-	private Sprite			sprite;
+    /** The permeability. */
+    private Permeability permeability;
 
-	public Element(Sprite sprite, Permeability permeability) {
-		this.setSprite(sprite);
-		this.setPermeability(permeability);
-	}
+    /** The sprite. */
+    private Sprite sprite;
 
-	@Override
-	public Image getImage() {
-		return this.getSprite().getImage();
-	}
+    /**
+     * Instantiates a new element.
+     *
+     * @param sprite
+     *            the sprite
+     * @param permeability
+     *            the permeability
+     */
+    public Element(final Sprite sprite, final Permeability permeability) {
+        this.setSprite(sprite);
+        this.setPermeability(permeability);
+    }
 
-	@Override
-	public Permeability getPermeability() {
-		return this.permeability;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see model.element.IElement#getImage()
+     */
+    @Override
+    public Image getImage() {
+        return this.getSprite().getImage();
+    }
 
-	@Override
-	public Sprite getSprite() {
-		return this.sprite;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see model.element.IElement#getPermeability()
+     */
+    @Override
+    public Permeability getPermeability() {
+        return this.permeability;
+    }
 
-	private void setPermeability(Permeability permeability) {
-		this.permeability = permeability;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see model.element.IElement#getSprite()
+     */
+    @Override
+    public Sprite getSprite() {
+        return this.sprite;
+    }
 
-	protected void setSprite(Sprite sprite) {
-		this.sprite = sprite;
-	}
+    /**
+     * Sets the permeability.
+     *
+     * @param permeability
+     *            the new permeability
+     */
+    private void setPermeability(final Permeability permeability) {
+        this.permeability = permeability;
+    }
+
+    /**
+     * Sets the sprite.
+     *
+     * @param sprite
+     *            the new sprite
+     */
+    protected void setSprite(final Sprite sprite) {
+        this.sprite = sprite;
+    }
 
 }
--- model/src/main/java/model/element/IElement.java
+++ model/src/main/java/model/element/IElement.java
@@ -5,13 +5,38 @@ import java.awt.Image;
 import model.element.motionless.MotionlessElement;
 import showboard.ISquare;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface IElement.
+ */
 public interface IElement extends ISquare {
+
+    /**
+     * Gets the sprite.
+     *
+     * @return the sprite
+     */
     public Sprite getSprite();
 
+    /**
+     * Gets the permeability.
+     *
+     * @return the permeability
+     */
     public Permeability getPermeability();
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see showboard.ISquare#getImage()
+     */
     @Override
     public Image getImage();
 
+    /**
+     * Creates the background.
+     *
+     * @return the motionless element
+     */
     public MotionlessElement createBackground();
 }
