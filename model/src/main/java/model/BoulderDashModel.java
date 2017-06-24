8ccb44e84c18a933c6585ca859570d233c454752
laure_anne <laureanne.marechal@viacesi.fr>
2017-06-24 18:56:36 +0200

    Javadoc

--- model/src/main/java/model/BoulderDashModel.java
+++ model/src/main/java/model/BoulderDashModel.java
@@ -7,35 +7,41 @@ import model.dao.DAO;
 import model.element.mobile.IMobile;
 
 public class BoulderDashModel implements IBoulderDashModel {
-	private IMap map;
+    private IMap map;
 
-	public BoulderDashModel(final int level) {
-		System.out.println("cc2");
-		this.setMap(new Map(level));
-	}
+    /**
+     * Instantiates a new boulder dash model.
+     *
+     * @param level
+     *            the level
+     */
+    public BoulderDashModel(final int level) {
+        System.out.println("cc2");
+        this.setMap(new Map(level));
+    }
 
-	@Override
-	public List<FillingMap> getAllPositionsById(final int levelID) throws SQLException {
-		return DAO.getMapFilledByID(levelID);
-	}
+    @Override
+    public List<FillingMap> getAllPositionsById(final int levelID) throws SQLException {
+        return DAO.getMapFilledByID(levelID);
+    }
 
-	@Override
-	public GamingMap getLevelByID(final int levelID) throws SQLException {
-		return DAO.getLevelByID(levelID);
-	}
+    @Override
+    public GamingMap getLevelByID(final int levelID) throws SQLException {
+        return DAO.getLevelByID(levelID);
+    }
 
-	@Override
-	public IMap getMap() {
-		return this.map;
-	}
+    @Override
+    public IMap getMap() {
+        return this.map;
+    }
 
-	@Override
-	public IMobile getMiner() {
-		return this.getMap().getMiner();
-	}
+    @Override
+    public IMobile getMiner() {
+        return this.getMap().getMiner();
+    }
 
-	private void setMap(final IMap map) {
-		this.map = map;
-	}
+    private void setMap(final IMap map) {
+        this.map = map;
+    }
 
 }
